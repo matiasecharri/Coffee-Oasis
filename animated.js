976bec768e3fba27fa9f7989e46ba2cf5ebe44b6
matiasecharri <matiasecharri@hotmail.com>
2023-01-22 21:05:18 -0300

    pink zone is working fine now, the 2 text (japanese and english) are working pretty good in simultanious

--- animated.js
+++ animated.js
@@ -1,5 +1,5 @@
 // Selecciona el elemento que deseas animar
-const animatedElement = document.querySelector('.animado');
+const animatedElement = document.querySelector('.animado' );
 
 // Crea el observer
 const observer = new IntersectionObserver(function(entries) {
@@ -7,13 +7,31 @@ const observer = new IntersectionObserver(function(entries) {
   if (entries[0].isIntersecting) {
     setTimeout(() => {
       animatedElement.classList.add('h4koi');
-    }, 100); // Retrasa la animación 2 segundos
+    }, 200); // Retrasa la animación 2 segundos
   }
 });
 
 console.log("saludos")
 // Comienza a observar el elemento
 observer.observe(animatedElement);
+
+
+
+const animatedElement2 = document.querySelector('.animado2' );
+
+// Crea el observer
+const observer2 = new IntersectionObserver(function(entries) {
+  // Comprueba si el elemento es visible
+  if (entries[0].isIntersecting) {
+    setTimeout(() => {
+      animatedElement2.classList.add('h5koi');
+    }, 1800); // Retrasa la animación 2 segundos
+  }
+});
+
+console.log("saludos")
+// Comienza a observar el elemento
+observer2.observe(animatedElement2);
 
 
 
