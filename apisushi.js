d59a0938605e711e2790ee164fcae58d6ae853d5
matiasecharri <matiasecharri@hotmail.com>
2023-01-31 16:58:04 -0300

    api creation in process

---
+++ apisushi.js
@@ -0,0 +1,297 @@
+let food = [
+  {
+    id: 01,
+    categorie: "roll",
+    type: "hot",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/r9mEYfq5doEz4yIPhxvJcFWLFRPyH6cP.jpg",
+    name: "Hot Bologna",
+    description:
+      "Breaded roll stuffed with prawns sautéed with paprika and Philadelphia.",
+    pricex5: 7,
+    sells: 15,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 02,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/y0PzxvPM822IgLpI6e5hHMbZwJReW2ku.jpg",
+    name: "New York Phila",
+    description:
+      "Roll pieces stuffed with salmon, avocado, and Philadelphia cheese.",
+    pricex5: 7.5,
+    sells: 50,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 03,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/aL7mGxv6yODE8M-1Ky9loHVoEaHxQ6dG.jpg",
+    name: "Nigiri Salmon",
+    description: "Rice snacks wrapped in fine cuts of the freshest salmon.",
+    pricex5: 9.5,
+    sells: 105,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 04,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/8rL5nMEZjBFS-PzwFXxeuaztf3-_E1kY.jpg",
+    name: "Aphrodite Bomb",
+    description:
+      "Salmon, mango, avocado and Philadelphia cheese wrapped in tamago, bathed in passion fruit sauce, topped with sweet potato fries.",
+    pricex5: 15.7,
+    sells: 205,
+    veggie: false,
+    sweet: true,
+  },
+  {
+    id: 05,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/EOxVfztVgkMEHgvdNHRRLToXq-_-k5Tt.jpg",
+    name: "Andino",
+    description:
+      "Roll stuffed with green beans, avocado and carrots with a tomato top topped with chives, crunchy walnuts and teri sauce.",
+    pricex5: 6,
+    sells: 20,
+    veggie: true,
+    sweet: false,
+  },
+  {
+    id: 06,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/zP1VZu9JTF02UrqRcUubRUeXZs-02cuu.jpg",
+    name: "Chimi Nikkei",
+    description:
+      "Roll stuffed with cherry tomatoes, tempura green beans and carrots with cheese topped with chimi nikkei.",
+    pricex5: 6,
+    sells: 100,
+    veggie: true,
+    sweet: false,
+  },
+  {
+    id: 07,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/B-vQf7o6s2qfYjcP5AsPofMohKAH3nbi.jpg",
+    name: "Fresh",
+    description:
+      "Roll stuffed with cheese, crispy mushrooms and onion pickles covered.",
+    pricex5: 7.7,
+    sells: 50,
+    veggie: true,
+    sweet: false,
+  },
+  {
+    id: 08,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/mbk0WTtykZQVSDFREIRirN0oXvUaMn06.jpg",
+    name: "Spring",
+    description:
+      "Roll stuffed with green onion, crispy onion and avocado topped with spring sauce.",
+    pricex5: 6,
+    sells: 250,
+    veggie: true,
+    sweet: false,
+  },
+  {
+    id: 09,
+    categorie: "roll",
+    type: "hot",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/5-u6qMSIGDDKfeKW23MKlA-hH_QFfYZO.jpg",
+    name: "Hot Firenze",
+    description:
+      "Breaded roll stuffed with prawns sautéed with paprika and Philadelphia.",
+    pricex5: 7,
+    sells: 15,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 10,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/5N-f-Vd9O56Qlfy4HHDMiDXq-a8tDzj3.jpg",
+    name: "Geishas Salmon",
+    description:
+      "Salmon Geishas stuffed with Phila cheese with Avocado.",
+    pricex5: 15.5,
+    sells: 250,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 11,
+    categorie: "roll",
+    type: "hot",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/_1T8ZAm4IZvXAygWSwD6-y-0YhOp-N01.jpg",
+    name: "Hot New York",
+    description:
+      "Breaded roll stuffed with salmon, rice, avocado and Phila cheese.",
+    pricex5: 8,
+    sells: 30,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 12,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/1KYIQK65yxVRuoi61QhTPBXInWh6Bo0F.jpg",
+    name: "Philadelphia",
+    description:
+      "Roll stuffed with salmon and Philadelphia cheese, covered with toasted sesame.",
+    pricex5:8.5,
+    sells: 350,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 13,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/q7-NBPdx7hKm-pIlewwJJ2oPaMCLv5a9.jpg",
+    name: "New York Classic",
+    description:
+      "Roll stuffed with salmon and avocado, covered with toasted sesame",
+    pricex5:8.5,
+    sells: 350,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 14,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/nJvuypdGIB6fEmQlXN5oq0QANmMkDiMV.jpg",
+    name: "Shrimp Vocado",
+    description:
+      "Roll stuffed with prawns coated in Philadelphia cheese, covered with avocado.",
+    pricex5:7.5,
+    sells: 350,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 15,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/lu24gtDcBY1C3er2yfd6L7hQjZQAvpMJ.jpg",
+    name: "Phila Tuna",
+    description:
+      "Classic Roll stuffed with tuna, mayonnaise, green, ground chili, and finlandia.",
+    pricex5:7.5,
+    sells: 60,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 16,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/KRis_040Keqw3p7AwbkG28z9BmgHgfxT.jpg",
+    name: "Buenos Aires",
+    description:
+      "Roll stuffed with Philadelphia cheese and battered prawns covered with a thin layer of salmon",
+    pricex5:7.8,
+    sells: 550,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 17,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/9MZFr7cxzHqeSCqHP_tgrb08Ki8rKhAI.jpg",
+    name: "Premium Sashimi",
+    description:
+      "Select cuts of the freshest salmon.",
+    pricex5:18.8,
+    sells: 1550,
+    veggie: false,
+    sweet: false,
+  },
+  {
+    id: 18,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/lugh3S-_j3UTFwDCVZ960cRiO5VpydRP.jpg",
+    name: "Premium Sashimi",
+    description:
+      "Roll stuffed with panko-coated prawns, with Philadelphia cheese, covered with chives and topped with sweet potato fries.",
+    pricex5:7.8,
+    sells: 90,
+    veggie: false,
+    sweet: true,
+  },
+  {
+    id: 19,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/9IOeyfMnwkcMUZY6m7QNkNtjPOY4Jv-Y.jpg",
+    name: "Paris",
+    description:
+      "Crepe-type roll stuffed with salmon, Philadelphia cheese, topped with peanut praline.",
+    pricex5:7.8,
+    sells: 20,
+    veggie: false,
+    sweet: true,
+  },
+  {
+    id: 20,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/hY7GpwqjA5TODMRy9xDh95I7NDLvCiP0.jpg",
+    name: "Philacuya",
+    description:
+      "Roll stuffed with avocado and Phila cheese, on the outside a fresh salmon tapa, crowned with fried sweet potato and passion fruit sauce.",
+    pricex5:6.8,
+    sells: 250,
+    veggie: false,
+    sweet: true,
+  },
+  {
+    id: 21,
+    categorie: "roll",
+    type: "cold",
+    photo:
+      "https://sushipopimg.s3.amazonaws.com/productos/MnpTDrens4xRxC7O428RxPqNQdxWvt-L.jpg",
+    name: "Napoli",
+    description:
+      "Roll stuffed with battered prawns and Philadelphia cheese with sweet and sour sauce of honey and mustard.",
+    pricex5:7.5,
+    sells: 250,
+    veggie: false,
+    sweet: true,
+  },
+];
+
+console.log(food);
