8847fe5e78add60344d90708d1be71fb932cd463
matiasecharri <matiasecharri@hotmail.com>
2023-05-28 17:05:16 -0300

    sushito

--- amenu.js
+++ amenu.js
@@ -11,76 +11,8 @@
 //Another Wordpress day
 //-----------------------------------------------------------//
 //CONSUMIENDO API----------------// CODE BLOCK 1
-async function consumiendoApi() {
-  let containerCards = document.getElementById("menumain");
-  containerCards.innerHTML = ` <div class="dot-spinner">
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-  <div class="dot-spinner__dot"></div>
-`;
-  let datosNipponBitesFetch = await fetch(
-    "https://my-json-server.typicode.com/matiasecharri/nippon-bites/products"
-  );
-  let datosNipponBites = await datosNipponBitesFetch.json();
-  printer(datosNipponBites);
+function sushiowo (){
 
-  //SEARCHBAR----------------// CODE BLOCK 2
-  function mySearchbar() {
-    //Declaro mi funcion simplemente para mantener organizado el código.
-    let userTextFilter = ""; // Creo una variable vacía donde posteriormente voy a guardar lo que escriba el usuario.
-    let searchbar = document.getElementById("searchbar1"); // Declaro una variable donde voy a traer mi searchbar.
-    searchbar.addEventListener("keyup", (x) => {
-      // Necesito que la searchbar responda a lo que se escribe asi que, addEventListener("keyup").
-      userTextFilter = x.target.value.toLowerCase(); // Le voy a decir que userTextFiltered va a ser igual al valor en tiempo real que tenga X o sea mi searchbar.
-      let datosFiltrados = datosNipponBites.filter((x) => {
-        // Voy a crear una variable donde se van a guardar los datos que filtre el usuario al escribir.
-        return x.name.toLowerCase().includes(userTextFilter); // Le voy a pedir que me retorne x.name(de mi array nippon) pero solo si incluye lo que el user escribió.
-      });
-      console.log(userTextFilter);
-      if (datosFiltrados.length === 0) {
-        //Le voy a decir que si datos filtrados es === 0 me de un mensaje de que eso no existe, porque si el array pasó a tener un .length de 0 es porque no hubo coincidencias entre lo que hay dentro de el y lo que escribio el usuario.
-        containerCards.innerHTML = `<p class="text-focus-in" >Sorry, nothing to show! <br> 
-      表示するものは何もありません。<p>`; //Aca le digo que quiero mostrarr en ese caso.
-      } else {
-        printer(datosFiltrados); //En cambio si hay coincidencia quiero que me muestre los datos filtrados.
-      }
-    });
-    searchbar.addEventListener("input", (x) => {
-      //Ademas tuve que agregar un addEventListener extra para que cuando el valor de la searchbar sea de ""
-      if (searchbar.value === "") {
-        //Es decir para cuando el usuario no escribió nada.
-        printer(datosNipponBites); //Esto le indica que si el usuario no escribe nada y searchbar sigue vacio tiene que imprimir todo.
-      }
-    });
-  }
-  mySearchbar();
-  //FIN SEARCHBAR-------------//
-}
-consumiendoApi();
-//IMPRESORA-------------------// CODE ssBLOCK 3
-function printer(array) {
-  let containerCards = document.getElementById("menumain");
-  containerCards.innerHTML = "";
-  array.forEach((x) => {
-    containerCards.innerHTML += `
-<div class="card">
-    <div class="cardimage">
-      <img src="${x.photo}">
-    </div>
-    <div class="descriptioncontainercard">
-    <h3>${x.name}</h3>
-    <p>${x.description}
-    </p>
-    <div class="pricecard"> Price: $${x.pricex5}</div>
-    <button> 私を食べなさい</button>
-  </div>
-  </div>`;
-  });
 }
 
 //Late Night Code; Started: 01:01, Finished 01:37 - Day 1/31
